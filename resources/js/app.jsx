312122ead9ce596b272cc1ade9f9d3460d39af1a
Wetende <cyprianwetende@gmail.com>
2025-07-08 16:14:47 +1000

    removed all vue.js depencies

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,9 +1,19 @@
-import '../css/app.css';
 import './bootstrap';
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
+
+// Libraries
+import { ParallaxProvider } from 'react-scroll-parallax';
+import { LazyMotion, domMax } from 'framer-motion';
+
+// CSS imports following Litho template structure
+import './Assets/css/icons.css';
+import './Assets/css/global.css';
+import './Assets/css/pages.css';
+import '../css/app.css';
+import './index.scss';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -17,7 +27,13 @@ createInertiaApp({
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(
+            <LazyMotion features={domMax}>
+                <ParallaxProvider>
+                    <App {...props} />
+                </ParallaxProvider>
+            </LazyMotion>
+        );
     },
     progress: {
         color: '#4B5563',
